317c0baa2a21b7f05a49bf0b9cbec9398afd70cf
Santilopezc <Santiago.lopez@ugent.be>
2024-11-23 23:54:28 -0500

    Update Dummy creation

--- backend/src/dummy_creation.py
+++ backend/src/dummy_creation.py
@@ -1,19 +1,31 @@
 import models
 import database
+
+# Drop the database
+database.Base.metadata.drop_all(bind=database.engine)
+
 # Create the database
 database.Base.metadata.create_all(bind=database.engine)
 # Create a session
 session = database.SessionLocal()
 
 # Create a dummy employee
-employee = models.Employee(
-    username="santiagouser",
+employee1 = models.Employee(
+    username="santiago",
     name="Santiago",
     position="Data Scientist",
     avatar_url="https://randomuser.me/api/portraits/men/1.jpg",
-    skills={"Python": 90, "SQL": 80, "Spark": 75, "ML": 85, "Stats": 70}
+    skills={"Python": 10, "SQL": 8, "Spark": 6, "Pytorch": 7, "ML": 8}
 )
-session.add(employee)
+employee2 = models.Employee(
+    username="factoredai",
+    name = "Alice",
+    position = "Software Engineer",
+    avatar_url = "https://randomuser.me/api/portraits/women/2.jpg",
+    skills = {"Python": 9, "SQL": 8, "React": 9, "CSS": 10, "JavaScript": 7}
+)
+session.add(employee1)
+session.add(employee2)
 session.commit()
 
 # Query all employees
